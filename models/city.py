34faf864924f19c77a8929519dc4942b1627a8c3
Eseoghene-ODODO <en.ododo@gmail.com>
2024-04-18 06:34:28 +0100

    task 8

--- models/city.py
+++ models/city.py
@@ -5,12 +5,9 @@ from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ City class definition """
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('state_id'), nullable=False)
-
-    def __init__(self, *args, **kwargs):
-        """Initialization method for City class"""
-        super().__init__(*args, **kwargs)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", cascade="all, delete", backref="cities")
--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel
 
 
@@ -11,3 +12,4 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
+    places = relationship("Place", cascade="all, delete", backref="user")
