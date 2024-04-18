0df3d304aab9de13fade976f235badb5104de28d
Eseoghene-ODODO <en.ododo@gmail.com>
2024-04-18 06:20:56 +0100

    task 7

--- models/state.py
+++ models/state.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, base
+from models.base_model import BaseModel
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class State(BaseModel):
-    """ State class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    if storage_type == 'db':
-        cities = relationship("City", cascade="all, delete", backref="state")
-
-    def __init__(self, *args, **kwargs):
-        """Initialization method for State class"""
-        super().__init__(*args, **kwargs)
+    """State class"""
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state", cascade="all, delete-orphan")
+    else:
+        @property
+        def cities(self):
+            """Getter attribute for cities in FileStorage"""
+            from models import storage
+            cities_list = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    cities_list.append(city)
+            return cities_list
